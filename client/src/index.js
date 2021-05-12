97f46561357ea398dd6fb8a6c5aa3e10367b8f49
Anferny Vanta <vantanferny@github.com>
2021-05-12 22:57:59 +0800

    💄 Add basic html structure

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,11 @@ import store from './redux';
 import App from './App';
 
 const render = Component => {
-    ReactDOM.render(
-        <Provider store={store}>
-            <Component />
-        </Provider>
-        , document.getElementById('cape-canaveral'));
+    // ReactDOM.render(
+    //     <Provider store={store}>
+    //         <Component />
+    //     </Provider>
+    //     , document.getElementById('cape-canaveral'));
 };
 
 render(App);
