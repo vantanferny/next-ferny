06ba65a10d84487ccd7709083c523c2b20e9b87a
Anferny Vanta <vantanferny@github.com>
2021-05-13 19:47:28 +0800

    :sparkles: Add ShipType filter
    Additional notes: Call server upon pressing search function

--- server/app.js
+++ server/app.js
@@ -19,10 +19,31 @@ app.use(function (_, res, next) {
 });
 
 app.get('/', async (req, res) => {
-    const rows = await dbPool.query('select * from ships;');
+    const query = `SELECT * FROM ships ${whereClause(req.query)};`
+
+    console.log('------------s')
+    console.log(query)
+    console.log('------------e')
+
+    const rows = await dbPool.query(query);
+
     res.status(200);
     res.send(rows);
 });
+
+const whereClause = (params) => {
+    if (Object.keys(params).length === 0) {
+        return ''
+    }
+
+    let clause = 'WHERE '
+
+    Object.keys(params).forEach(function(key) {
+        clause += `${key} = '${params[key]}' AND`
+    })
+
+    return clause.slice(0, -4) // remove last " AND"
+}
 
 app.listen('4000');
 console.log(`Listening on port: 4000, wait for the development server to be up...`);
