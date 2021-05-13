f19e7e3bf155e341a0c2bd45e02aee70a0c4f10a
Anferny Vanta <vantanferny@github.com>
2021-05-13 20:21:52 +0800

    :sparkles: Add sanitation, validation, and error notification

--- server/app.js
+++ server/app.js
@@ -19,11 +19,22 @@ app.use(function (_, res, next) {
 });
 
 app.get('/ships', async (req, res) => {
+    const validationResult = requestValidator(req.query)
+    if (!validationResult.success) {
+        res.send({
+            error: validationResult.error
+        });
+
+        return;
+    }
+
     const query = `SELECT * FROM ships ${whereClause(req.query)};`
     const rows = await dbPool.query(query);
 
     res.status(200);
-    res.send(rows);
+    res.send({
+        data: rows
+    });
 });
 
 const whereClause = (params) => {
@@ -38,6 +49,38 @@ const whereClause = (params) => {
     })
 
     return clause.slice(0, -4) // remove last " AND"
+}
+
+const requestValidator = (params) => {
+    if (Object.keys(params).length === 0) {
+        return {
+            success: true
+        }
+    }
+
+    if (params['type']) {
+        const validShipTypes = ['Tug', 'Cargo', 'Barge', 'High Speed Craft']
+
+        if (!validShipTypes.includes(params['type'])) {
+            return {
+                success: false,
+                error: 'Ship type invalid.'
+            }
+        }
+    }
+
+    if (params['weight']) {
+        if (isNaN(params['weight'])) {
+            return {
+                success: false,
+                error: 'Weight should be a number.'
+            }
+        }
+    }
+
+    return {
+        success: true
+    }
 }
 
 app.listen('4000');
